39136205653e5a2f549d2dad82164309870170e7
Timur <tumyp.paxumob@gmail.com>
2022-07-07 00:28:56 +0500

    Added 013 and 014

---
+++ Examples013_String/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string text = "Дождь в нашей выжженной солнцем степи был редким и долгожданным гостем."
+        + " Можно даже сказать, почётны.Все уже смотрели на небо в надежде увидеть хоть стайку"
+        + " дождевых облачков. Когда же будет дождь? Пусть он придёт и землю хоть немного обольёт."
+        + " Но небо чисто. Солнце высоко. Жара. И вот — о чудо! Пришёл он к нам однажды после обеда,"
+        + " чуть к вечеру, как раз к полднику.";
+
+
+        string Replace(string text, char oldValue, char newValue)
+        {
+            string result = string.Empty;
+
+            int length = text.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (text[i] == oldValue) result += newValue;
+                else result += text[i];
+            }
+
+            return result;
+        }
+        string newText = Replace(text, ' ', '-');
+        Console.WriteLine(newText);
+        Console.WriteLine();
+        newText = Replace(newText, 'и', 'i');
+        Console.WriteLine(newText);
+        Console.WriteLine();
+    }
+}
---
+++ Examples014_Array_Sorting/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int [] arr = {1, 5, 4, 8, 3, 2, 7, 1, 5, 9, 2, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} "); // (array[i] + " ")
+    }
+Console.WriteLine();
+Console.WriteLine();
+}
+
+void SortingArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SortingArray(arr);
+
+PrintArray(arr);
+
